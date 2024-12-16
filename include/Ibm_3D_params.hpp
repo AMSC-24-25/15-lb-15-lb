55292abd7fb145469b973f5bc14b5bf0a276dd79
qiansong li <qiansong.li@mail.polimi.it>
2024-12-16 07:13:51 +0100

    Update Ibm_3D_params.hpp

--- include/Ibm_3D_params.hpp
+++ include/Ibm_3D_params.hpp
@@ -1,20 +1,20 @@
 #ifndef LBM_PARAMS_HPP
 #define LBM_PARAMS_HPP
 
-namespace D3Q19{
-    const int Q = 19;  // D3Q19速度方向数
-    const int Nx = 100; 
-    const int Ny = 100; // 网格尺寸 (y方向)
-    const int Nz = 100; // 网格尺寸 (z方向)
-    const double tau = 0.6; // 松弛时间
-    const double U = 0.1;   // 顶部速度 (边界条件)
-}
+const int Q = 19;  // D3Q19速度方向数
+const int Nx = 100; 
+const int Ny = 100; // 网格尺寸 (y方向)
+const int Nz = 100; // 网格尺寸 (z方向)
+const double tau = 0.6; // 松弛时间
+const double U = 0.1;   // 顶部速度 (边界条件)
+const double omega = 1.0 / tau;             // 弛豫因子
 
 const int c_D3Q19[19][3] = {
     {0, 0, 0}, {1, 0, 0}, {-1, 0, 0}, {0, 1, 0}, {0, -1, 0}, {0, 0, 1}, {0, 0, -1},
     {1, 1, 0}, {-1, -1, 0}, {1, -1, 0}, {-1, 1, 0}, {1, 0, 1}, {-1, 0, -1},
     {1, 0, -1}, {-1, 0, 1}, {0, 1, 1}, {0, -1, -1}, {0, 1, -1}, {0, -1, 1}
 };
+
 
 const double w_D3Q19[19] = {
     1.0 / 3.0,   // 0
@@ -23,8 +23,13 @@ const double w_D3Q19[19] = {
     1.0 / 36.0, 1.0 / 36.0, 1.0 / 36.0, 1.0 / 36.0, 1.0 / 36.0, 1.0 / 36.0
 };
 
-inline int idx_D3Q19(int x, int y, int z, int k, int Nx, int Ny, int Nz) {
-    return ((z * D3Q19::Ny + y) * D3Q19::Nx + x) * D3Q19::Q + k;
+inline size_t idx_D3Q19(int i, int j, int z, int k, int Nx, int Ny) {
+    return static_cast<size_t>(((z * Ny + j) * Nx + i) * 19 + k);
 }
+
+inline size_t idx_D3Q19(int i, int j, int z, int Nx, int Ny) {
+    return static_cast<size_t> ((z * Ny + j) * Nx + i);
+}
+
 
 #endif
